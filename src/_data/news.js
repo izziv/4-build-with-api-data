c7867c1c3c092b56fb9d625ed9e7e5f942aca1e9
izziv <spozzy@gmail.com>
2023-02-17 00:03:50 -0500

    adding dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,12 @@
+
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2023-02-16&to=2023-02-16&sortBy=popularity&apiKey=${process.env.API_KEY}&pageSize=10`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
