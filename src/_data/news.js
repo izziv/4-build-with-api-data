a7c954e5e62c7591f3a045eaf19ba8c96a0c8f93
izziv <spozzy@gmail.com>
2023-02-17 00:20:43 -0500

    Improved ENV variable name

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2023-02-16&to=2023-02-16&sortBy=popularity&apiKey=${process.env.API_KEY}&pageSize=10`);
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2023-02-16&to=2023-02-16&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=10`);
       return response.data;
     } catch (error) {
       console.error(error);
